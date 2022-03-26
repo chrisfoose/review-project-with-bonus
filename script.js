4fc476439d37008821291961d9c896437137c525
Christopher Foose <chrisfoose@gmail.com>
2022-03-26 15:20:46 -0700

    Got the array working, just filter out commas.  So close.

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 // Start your code ***HERE*** =========
 
-const password = document.getElementById("password");
+//const password = document.getElementById("password");
 
 // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
 
-const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const pass = '';
+const chars = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h'];
+let passArr = [];
 
 
 // create a global variable called "pwLength" with a number between 10 and 18
 
-const pwLength = 18;
+const pwLength = 50;
 
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
 
 function addNewPassword() {
-for (let i = 0; i < pass.length; i++) {
-    const randomNumber = Math.floor(Math.random() * chars.length);
-    pass += chars.substring(randomNumber, randomNumber + 1);
+for (let i = 0; i < 18; i++) {
+     pass = chars[Math.floor(Math.random() * chars.length)];
+     passArr.push(pass);
+  
     };
 
-    document.getElementById("password").value = 'pass';
-    return pass;
+return passArr;    //document.getElementById("password").value = 'pass';
+    
 };
 
-function copyPassword() {
+/* function copyPassword() {
     const copyText = document.getElementById('password');
     copyText.select();
     document.execCommand("copy");
-}
+} */
 
 // ========= ⬇ DO NOT TOUCH THIS CODE ⬇ ======
 
