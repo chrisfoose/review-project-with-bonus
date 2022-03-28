05719e942bd0fcde2d71cd1dab55f1477f0a4f1f
Christopher Foose <chrisfoose@gmail.com>
2022-03-27 20:23:21 -0700

    cleared those fields per click

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Start your code ***HERE*** =========
 
+//document.getElementById("password").value = "";
+
 //const password = document.getElementById("password");
 
 // create an array with all possible character types: Uppercase, Lowercase, numbers, and symbols stored in a variable
@@ -13,6 +15,14 @@ let passArr = [];
 let pwLength = 18;
 
 // Using the above array and password length variable, create a random password using a for loop inside of a function called "addNewPassword" either saved as an arrow function variable or a traditional function
+
+// function to clear fields
+
+function clearFields() {
+    //document.getElementById("password").value = "";
+    passArr = [];
+};
+
 
 //function to get character length
 
@@ -29,11 +39,13 @@ let pwLength = 18;
 charLength(); 
 
 function addNewPassword() {
-for (let i = 0; i < pwLength; i++) {
-     pass = chars[Math.floor(Math.random() * chars.length)];
-     passArr.push(pass);
+    clearFields(); //clear those fields!
+    for (let i = 0; i < pwLength; i++) {
+         pass = chars[Math.floor(Math.random() * chars.length)];
+        passArr.push(pass);
      
 }
+
 return passArr.join(''); 
 //let joinPassArr = passArr.join(' ');
    
@@ -41,6 +53,8 @@ return passArr.join('');
 //document.getElementById("password").value = 'pass';
     
 };
+
+
 
 /* function copyPassword() {
     const copyText = document.getElementById('password');
@@ -52,6 +66,7 @@ return passArr.join('');
 
 let genBtn = document.getElementById("btnGen");
 let buttonHandler = () => {
+   
     document.getElementById("password").value = addNewPassword();
 };
 // Event listener for generate PW button
